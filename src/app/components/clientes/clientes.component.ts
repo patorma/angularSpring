54cbc43154586f412aa52b0e4162494e062458b7
patorma <pocontreras@ing.ucsc.cl>
2020-01-11 03:05:08 -0300

    se implemento el delete y se arreglo layout de footer

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from '../../services/cliente.service';
+import {  Router,ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
 
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
 
 
   clientes: Cliente[];
+  
 
 
   /* Inyección de dependencias*/
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService, private router: Router ,private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     /*clientes es un observador va hacer observado por observadores, aca se subscribe , y en el metodo subscribe el observador seria asignar el atributo clientes el valor que se recibe del clienteservice, que seria el listado de clientes con los cambios*/
@@ -24,6 +26,57 @@ export class ClientesComponent implements OnInit {
        /*nuestro observador, actualiza estado de clientes*/
        clientes => this.clientes = clientes
      );
+  }
+
+  public delete(cliente: Cliente): void {
+
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Está seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response =>{
+            this.clientes = this.clientes.filter(cli =>cli !== cliente)
+            swalWithBootstrapButtons.fire(
+              'Cliente eliminado!',
+              `Cliente ${cliente.nombre} eliminado con éxito.`,
+              'success'
+            )
+          }
+        )
+       
+      } 
+    })
+    /*this.activatedRoute.params.subscribe(params =>{
+      let id = params['id']
+      if(id){
+        this.clienteService.delete(id).subscribe(
+           cliente => {
+            
+             this.cliente = cliente;
+             this.router.navigate(['/clientes'])
+            
+
+            }
+        )
+      }
+    }
+
+    )*/
   }
 
 }
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -28,10 +28,14 @@ export class ClienteService {
   }
 
   getCliente(id): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
   }
 
   update(cliente: Cliente): Observable<Cliente> {
-    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente,{headers: this.httpHeaders})
+    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
+  }
+
+  delete(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`,{headers: this.httpHeaders});
   }
 }
