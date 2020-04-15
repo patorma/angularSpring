832ab31e6e47e14a8f404fd4b6648d903bcece67
patorma <pocontrera@ing.ucsc.cl>
2020-04-15 18:13:37 -0400

    boton de primera y ultima pagina , ngOnChanges *se configuro boton de primera y ultima pagina * se vio el modulo ngOnChanges que soluciona el problema de que se invoca una sola vez el paginador en ngOnInit * se vio la aplicacion del metodo ngOnChanges()

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit , Input, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
@@ -6,20 +6,41 @@ import { Component, OnInit , Input} from '@angular/core';
   styles: [
   ],
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
 
  @Input() paginador: any;
 
  paginas: number[];
+
+ desde: number;
+ hasta: number;
    
   constructor() { }
-
+// ngOnInit() se ejecuta solo una vez
   ngOnInit(): void {
+  
+  }
+
+  ngOnChanges(){
+      // se calcula el desde y hata para calcular el rango a mostrar en el paginador
+    // en desde habran dos calculos
+    // primer parametro de desde el maximo entre  y nuestra pagina actual menos 4 y segundo parametro totaldepaginas-5
+    // min [max (el minimo que podria tener el desde  y la pagina actual-4)]
+    // la pagina actual la obtenemos del paginador a traves del atributo number
+    this.desde = Math.min( Math.max(1, this.paginador.number-4), this.paginador.totalPages-5);
+    // max[el minimo entre el total de paginas y nuestra pagina actual + 4]
+    this.hasta = Math.max(Math.min(this.paginador.totalPages,this.paginador.number+4), 6);
+
+    if(this.paginador.totalPages > 5){
+      this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((_valor,indice) => indice + this.desde);
+    } else {
     //  en el contructor(Array) el numero de elementos, 
     // totalPages(este atributo contiene el total de paginas 
     // que existe dentro de los atributos del  paginador) 
     // se llena este arreglo con un valor por defecto
     this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor,indice) => indice + 1);
+  }
+
   }
 
 }
