a01637a7d052a722d7d9e5b2d6a297d1d9d74324
patorma <pocontreras@ing.ucsc.cl>
2020-05-11 01:46:49 -0400

    se agrego angular material al proyecto para ingresar la fecha en el formulario del cliente en el frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 // modulo cliente
-import { ClientesComponent } from './components/clientes/clientes.component';
-import { FormComponent } from './components/clientes/form.component';
-import { PaginatorComponent } from './components/paginator/paginator.component';
+import { ClientesComponent } from "./components/clientes/clientes.component";
+import { FormComponent } from "./components/clientes/form.component";
+import { PaginatorComponent } from "./components/paginator/paginator.component";
 
-import { ClienteService } from './services/cliente.service';
-import {HttpClientModule} from '@angular/common/http';
+import { ClienteService } from "./services/cliente.service";
+import { HttpClientModule } from "@angular/common/http";
 
-import { FormsModule } from '@angular/forms';
-import { ClienteBuscadoComponent } from './components/cliente-buscado/cliente-buscado.component';
+import { FormsModule } from "@angular/forms";
+import { ClienteBuscadoComponent } from "./components/cliente-buscado/cliente-buscado.component";
 //import es from '@angular/common/locales/es';
-import localeEsAr from '@angular/common/locales/es-AR';
-import { registerLocaleData } from '@angular/common';
-import localeES from '@angular/common/locales/es-CL';
+import localeEsAr from "@angular/common/locales/es-AR";
+import { registerLocaleData } from "@angular/common";
+import localeES from "@angular/common/locales/es-CL";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-registerLocaleData(localeES, 'es-CL');
+// maneja fechas
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+
+registerLocaleData(localeES, "es-CL");
 //registerLocaleData(localeEsAr, 'es-AR');
 @NgModule({
   declarations: [
@@ -31,15 +36,18 @@ registerLocaleData(localeES, 'es-CL');
     ClientesComponent,
     FormComponent,
     ClienteBuscadoComponent,
-    PaginatorComponent
+    PaginatorComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
   ],
-  providers: [ClienteService, { provide: LOCALE_ID, useValue: 'es-CL' }],
-  bootstrap: [AppComponent]
+  providers: [ClienteService, { provide: LOCALE_ID, useValue: "es-CL" }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
